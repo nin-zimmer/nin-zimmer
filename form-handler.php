8cf05448017bbfc4d8b42a55ca19b73826583f60
nin-zimmers <111365605+nin-zimmer@users.noreply.github.com>
2023-04-08 12:42:00 +0200

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info{yourwebname.com';
+$email_from = 'info@nin-zimmer.github.io';
 
 $email_subject = 'Neukunde';
 
